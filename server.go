4931b865fb18d993df5a4636bb96f034c8ca2b9c
WesleySouzaDev <wesleydesouza501@gmail.com>
2026-07-23 00:15:22 -0300

    feat: add config package with environment definitions

--- server.go
+++ server.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/WesleySouzaDev/cv-manager/config"
 	"github.com/WesleySouzaDev/cv-manager/generated"
 	"github.com/WesleySouzaDev/cv-manager/resolvers"
 	"github.com/go-chi/chi/v5"
@@ -17,12 +17,11 @@ import (
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
-const defaultPort = "8080"
-
 func main() {
-	port := os.Getenv("PORT")
+	env := config.GetEnv()
+	port := env.Port
 	if port == "" {
-		port = defaultPort
+		port = config.DEFAULT_PORT
 	}
 
 	router := chi.NewRouter()
